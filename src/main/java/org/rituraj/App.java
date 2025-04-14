85a49cb3c6b46180cdd69c08df93d2adaf99d3ca
Rituraj2610 <26rituraj@gmail.com>
2025-04-14 13:30:17 +0530

    [Rituraj] Add . UC1: Loading driver and setting up the connection

--- src/main/java/org/rituraj/App.java
+++ src/main/java/org/rituraj/App.java
@@ -1,13 +1,37 @@
 package org.rituraj;
 
-/**
- * Hello world!
- *
- */
+import java.sql.*;
+import java.util.Enumeration;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args )  {
+
+        // UC1: Loading the driver and setting up the connection
+        String url = "jdbc:mysql://localhost:3306/payroll_service";
+        String user = "root";
+        String pswd = "root";
+        Connection con;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded");
+        }catch (ClassNotFoundException e){
+            throw new IllegalStateException("Driver not found");
+        }
+
+            Enumeration<Driver> drivers =  DriverManager.getDrivers();
+            if(drivers.hasMoreElements()){
+                System.out.println(drivers.nextElement());
+            }
+
+
+        try{
+            con = DriverManager.getConnection(url, user, pswd);
+            System.out.println("Connection success");
+            }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+
     }
 }
