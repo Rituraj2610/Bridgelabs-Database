8243032790ebe15a69dfc7bf4c1dd7be5eb25322
Rituraj2610 <26rituraj@gmail.com>
2025-04-16 13:32:07 +0530

    [Rituraj] ADD . Generate fare

---
+++ src/test/java/org/rituraj/InvoiceGeneratorTest.java
@@ -0,0 +1,15 @@
+package org.rituraj;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceGeneratorTest {
+
+    @Test
+    public void givenDistanceAndTime_shouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double fare = invoiceGenerator.calculateFare(2.0, 5);
+        assertEquals(25.0, fare); // 2*10 + 5*1 = 25
+    }
+}
+
